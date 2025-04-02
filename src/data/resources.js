66389c50606b7488af60e35f105e3e38ae1baea4
Stephen Foster <stephen@thoughtstem.com>
2025-04-02 09:55:07 -0700

    ai ambassador searchability

--- src/data/resources.js
+++ src/data/resources.js
@@ -22,74 +22,74 @@ export default [
   },
   */
   {
-    "tags": ["for-faculty", "video", "discussion"],
+    "tags": ["for-faculty", "video", "discussion", "ai-ambassadors"],
     "dateCreated": "2025-03-20",
     "title": "The AI Natives are Coming",
     "slug": "ai-natives",
     "author": ["stephen-foster", "lindsey-handley"],
-    "content": "Conjecture and discussion about the eventuality of 'AI Natives'\n\nhttps://youtu.be/UebuD_n7w4s\n\nIn just a few years, students who have been using AI throughout their high school years will be entering college.  What will this mean for us as educators?  How will we adapt to this new reality?  What are the implications for our students and their learning?  This is a discussion about these questions and more."
+    "content": "Conjecture and discussion about the eventuality of 'AI Natives'\n\nhttps://youtu.be/UebuD_n7w4s\n\nIn just a few years, students who have been using AI throughout their high school years will be entering college.  What will this mean for us as educators?  How will we adapt to this new reality?  What are the implications for our students and their learning?  This is an AI Ambassador workshop discussion about these questions and more."
   },
   {
-    "tags": ["for-faculty", "video", "discussion"],
+    "tags": ["for-faculty", "video", "discussion", "ai-ambassadors"],	
     "dateCreated": "2025-03-15",
     "title": "DeepSeek vs ChatGPT",
     "slug": "deepseek-vs-chatgpt",
     "author": ["stephen-foster", "lindsey-handley"],
-    "content": "A hands-on, informal investigation of DeepSeek (vs ChatGPT)\n\nhttps://youtu.be/v4sSUEGIFMM\n\nIn this video, we explore the differences between DeepSeek and ChatGPT.  We investigate the strengths and weaknesses of each tool, and how they can be used in different contexts.  Although our investigation is limited and anecdotal, we conclude that Deep Seek seems to be just as good, if not occasionally better than, ChatGPT."
+    "content": "A hands-on, informal investigation of DeepSeek (vs ChatGPT)\n\nhttps://youtu.be/v4sSUEGIFMM\n\nIn this AI Ambassador workshop, we explore the differences between DeepSeek and ChatGPT.  We investigate the strengths and weaknesses of each tool, and how they can be used in different contexts.  Although our investigation is limited and anecdotal, we conclude that Deep Seek seems to be just as good, if not occasionally better than, ChatGPT."
   },
   {
-    "tags": ["video", "presentation", "for-faculty"],
+    "tags": ["video", "presentation", "for-faculty", "ai-ambassadors"],	
     "dateCreated": "2023-10-01",
     "title": "AI for Creating Lesson Plans",
     "slug": "ai-for-lesson-plans",
     "author": ["justin-lewis","lindsey-handley","stephen-foster"],
-    "content": "A 40-minute presentation from Fall 2023:\n\nhttps://youtu.be/h78cdBBzPMQ\n\nThis presentation covers how to use AI to create lesson plans.  We discuss the benefits and drawbacks of using AI in this context, and provide examples of how it can be used effectively."
+    "content": "An AI Ambassador presentation from Fall 2023:\n\nhttps://youtu.be/h78cdBBzPMQ\n\nThis presentation covers how to use AI to create lesson plans.  We discuss the benefits and drawbacks of using AI in this context, and provide examples of how it can be used effectively."
   },
   {
-    "tags": ["video", "presentation", "for-faculty"],
+    "tags": ["video", "presentation", "for-faculty", "ai-ambassadors"],	
     "dateCreated": "2023-10-01",
     "title": "Using AI in the Classroom",
     "slug": "ai-in-the-classroom",
     "author": ["justin-lewis"],
-    "content": "A 55-minute presentation from Fall 2023:\n\nhttps://youtu.be/b-ZjbBYFJuQ\n\nThis presentation covers how to use AI in the classroom.  We discuss the benefits and drawbacks of using AI in this context, and provide examples of how it can be used effectively."
+    "content": "An AI Ambassador presentation from Fall 2023:\n\nhttps://youtu.be/b-ZjbBYFJuQ\n\nThis presentation covers how to use AI in the classroom.  We discuss the benefits and drawbacks of using AI in this context, and provide examples of how it can be used effectively."
   },
   {
-    "tags": ["video", "presentation", "for-faculty"],
+    "tags": ["video", "presentation", "for-faculty", "ai-ambassadors"],
     "dateCreated": "2023-10-01",
     "title": "Teaching writing with AI",
     "slug": "ai-for-writing",
     "author": ["justin-lewis"],
-    "content": "A 40-minute presentation from Fall 2023:\n\nhttps://youtu.be/J81EK6Pnl14\n\nThis presentation covers how to use AI to teach writing.  This is an important and difficult topic in an age where AI can generate text that is often superior to what the average undergraduate college student can produce alone."
+    "content": "An AI Ambassador presentation from Fall 2023:\n\nhttps://youtu.be/J81EK6Pnl14\n\nThis presentation covers how to use AI to teach writing.  This is an important and difficult topic in an age where AI can generate text that is often superior to what the average undergraduate college student can produce alone."
   },
   {
-    "tags": ["video", "presentation", "for-faculty"],
+    "tags": ["video", "presentation", "for-faculty", "ai-ambassadors"],
     "dateCreated": "2023-10-01",
     "title": "AI Basics for Faculty",
     "slug": "ai-basics",
     "author": ["stephen-foster"],
-    "content": "A 40-minute presentation from Fall 2023:\n\nhttps://youtu.be/GFfTXSy5Cac\n\nThis presentation covers the basics of AI, including what it is, how it works, and its potential applications in education.  We demonstrate how to use ChatGPT, as well as some basic prompt engineering."
+    "content": "An AI Ambassador presentation from Fall 2023:\n\nhttps://youtu.be/GFfTXSy5Cac\n\nThis presentation covers the basics of AI, including what it is, how it works, and its potential applications in education.  We demonstrate how to use ChatGPT, as well as some basic prompt engineering."
   },
   {
-    "tags": ["video", "presentation", "for-faculty"],
+    "tags": ["video", "presentation", "for-faculty", "ai-ambassadors"],	
     "dateCreated": "2023-10-01",
     "title": "AI Basics for Faculty",
     "slug": "prompt-engineering-basics",
     "author": ["justin-lewis"],
-    "content": "A 40-minute presentation from Fall 2023:\n\nhttps://youtu.be/Reo3fXorLBM\n\nThis presentation covers the basics of prompt engineering, including what it is, how it works, and advice for educators who wish to use AI or to teach their students how to use it."
+    "content": "An AI Ambassador presentation from Fall 2023:\n\nhttps://youtu.be/Reo3fXorLBM\n\nThis presentation covers the basics of prompt engineering, including what it is, how it works, and advice for educators who wish to use AI or to teach their students how to use it."
   },
   {
-    "tags": ["video", "presentation", "for-faculty"],
+    "tags": ["video", "presentation", "for-faculty", "ai-ambassadors"],
     "dateCreated": "2023-10-01",
     "title": "AI Panel",
     "slug": "ai-panel-discussion",
     "author": ["stephen-foster", "lindsey-handley", "justin-lewis"],
-    "content": "A 45-minute AI panel discussion from Fall 2023:\n\nhttps://youtu.be/eHiKEkuYz2U\n\nThis panel discussion covers a variety of topics related to AI, including its potential applications in education, its limitations, and its ethical implications.  We also discuss the future of AI and its impact on society."
+    "content": "An AI panel discussion from Fall 2023:\n\nhttps://youtu.be/eHiKEkuYz2U\n\nThis AI Ambassador workshop covers a variety of topics related to AI, including its potential applications in education, its limitations, and its ethical implications."
   },
   {
     "tags": ["for-faculty", "software"],
     "dateCreated": "2023-06-01",
     "title": "AI-powered Textbooks",
-    "slug": "ai-textbookx",
+    "slug": "ai-textbook",
     "author": ["stephen-foster"],
     "content": AIPoweredTextbooks
   }
